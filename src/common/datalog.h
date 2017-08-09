26eb0fef7b2928ab9238709d088e034ba5bca3b8
Matthias Kubisch <kubisch@informatik.hu-berlin.de>
2017-08-09 23:06:11 +0200

    added data logger interface, and adapted it to simloid.

--- src/common/datalog.h
+++ src/common/datalog.h
@@ -37,4 +37,40 @@ private:
     bool enabled;
 };
 
+
+template <unsigned BufferSize = 1024>
+class Loggable {
+
+    char buffer[BufferSize];
+    std::size_t total_bytes = 0;
+
+protected:
+
+    template<typename... Args>
+    void append(const char* format, const Args&... args)
+    {
+        if (total_bytes + 1 < BufferSize) {
+            auto n = snprintf(buffer+total_bytes, BufferSize-total_bytes, format, args...);
+            if (n < 0) err_msg(__FILE__,__LINE__," Encoding error.");
+            if (n < BufferSize-total_bytes) {
+                total_bytes += n;
+                return;
+            }
+        }
+        err_msg(__FILE__,__LINE__," Buffer size of %u too small.", BufferSize);
+    }
+
+    const char* done(void) {
+        //dbg_msg("Bytes written %u", total_bytes);
+        total_bytes = 0;
+        return buffer;
+    }
+
+public:
+    virtual ~Loggable() {}
+    virtual const char* log(void) = 0;
+
+};
+
+
 #endif // DATALOG_H_INCLUDED
---
+++ src/robots/simloid_log.h
@@ -0,0 +1,43 @@
+/*---------------------------------+
+ | Matthias Kubisch                |
+ | kubisch@informatik.hu-berlin.de |
+ | July 2017                       |
+ +---------------------------------*/
+
+#ifndef SIMLOID_LOG_H_INCLUDED
+#define SIMLOID_LOG_H_INCLUDED
+
+#include <sstream>
+#include <robots/simloid.h>
+
+namespace robots {
+
+class Simloid_Log : public Loggable<768> {
+
+    const Simloid& simloid;
+
+public:
+    Simloid_Log(const Simloid& simloid) : simloid(simloid) {}
+
+    const char* log()
+    {
+        for (auto const& j : simloid.get_joints())
+            append("%+e %+e %+e ", j.s_ang, j.s_vel, j.motor.get());
+
+        for (auto const& a : simloid.get_accels())
+            append("%+e %+e %+e ", a.v.x, a.v.y, a.v.z);
+
+        append( "%+e %+e %+e %+e %+e"
+              , simloid.get_avg_rotation_inf_ang()
+              , simloid.get_avg_rotational_speed()
+              , simloid.get_avg_velocity_forward()
+              , simloid.get_avg_velocity_left()
+              , simloid.get_normalized_mechanical_power() );
+
+        return done();
+    }
+};
+
+} // namespace robots
+
+#endif // SIMLOID_LOG_H_INCLUDED
