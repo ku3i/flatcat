f87ebd6cffe36d21473ddcfc21ad05cb69b6ceba
Matthias Kubisch <kubisch@informatik.hu-berlin.de>
2017-12-02 07:58:09 +0100

    Unlocking controllers.

--- src/midi/midi_in.h
+++ src/midi/midi_in.h
@@ -15,24 +15,36 @@
     g++ -Wall -D__LINUX_ALSA__ -o test test.cpp RtMidi.cpp -lasound -lpthread
 */
 
+/* [0,1] -> [-1,1] */
+inline double normed(double value) { return 2 * value - 1; }
+
 class MidiIn
 {
+    struct Data_t {
+        Data_t() : raw(.0), unlocked(false) {}
+        double raw;
+        mutable bool unlocked;
+    };
+
     static constexpr std::size_t max_channel = 256;
-    static constexpr double      default_val = 0.0;
+    const Data_t      default_val = Data_t{};
 
     std::unique_ptr<RtMidiIn>  midi_ptr;
     std::vector<unsigned char> message;
-    std::vector<double>        data;
+    std::vector<Data_t>        data;
+
     std::string                interface_name;
     bool                       init_success;
+    bool                       verbose;
 
 public:
-    MidiIn(unsigned port)
+    MidiIn(unsigned port, bool verbose = true)
     : midi_ptr(nullptr)
     , message()
     , data(max_channel, default_val)
     , interface_name()
     , init_success(false)
+    , verbose(verbose)
     {
         assert(data.size() == max_channel);
         try {
@@ -73,8 +85,9 @@ public:
 
             if (num_bytes >= 2) {
                 const unsigned char channel = message[1];
-                data[channel] = static_cast<double>(message[2] / 127.0);
-                dbg_msg("MIDI(%03u): ch=%03u val=%4.2f dt=%4.2f", message[0], message[1], data[channel], dt);
+                data[channel].raw = static_cast<double>(message[2] / 127.0);
+                if (verbose)
+                    dbg_msg("MIDI(%03u): ch=%03u val=%4.2f dt=%4.2f", message[0], message[1], data[channel].raw, dt);
             }
 
         }
@@ -86,8 +99,19 @@ public:
         return true;
     }
 
-    double operator[] (std::size_t index) const { return data.at(index); }
+    double operator[] (std::size_t index) const { return data.at(index).raw; }
 
+    /* normed and unlocked */
+    double get(std::size_t index, double unlock_initial = 0.0) const {
+        const double val = normed(data.at(index).raw);
+
+        if (data.at(index).unlocked) return val;
+        else if (std::abs(val - unlock_initial) <= 1/127.) { /* check if we can unlock */
+            data.at(index).unlocked = true;
+            return val;
+        }
+        else return .0;
+    }
 };
 
 
