bb641c3fab670d2e7f370166fe3d0a3ad1b87b24
Matthias Kubisch <kubisch@informatik.hu-berlin.de>
2020-08-10 11:17:52 +0200

    adding homeokinetic predictormodule.

---
+++ src/learning/homeokinetic_predictor.h
@@ -0,0 +1,96 @@
+#ifndef HOMEOKINETIC_PREDICTOR_H
+#define HOMEOKINETIC_PREDICTOR_H
+
+#include <control/sensorspace.h>
+#include <learning/predictor.h>
+#include <learning/homeokinesis.h>
+
+namespace learning {
+
+/*--------------------------------------------+
+ | Homeokinetic Prediction and Control Core   |
+ +--------------------------------------------*/
+
+class Homeokinetic_Core : public Predictor_Base
+{
+    Homeokinetic_Control core;
+
+    // settings
+    unsigned context = 4;
+
+    Homeokinetic_Core(const Homeokinetic_Core& other) = delete;
+    Homeokinetic_Core& operator=(const Homeokinetic_Core& other) = delete;
+
+public:
+
+    Homeokinetic_Core( sensor_input_interface const& input
+                     , std::size_t number_of_joints
+                     , double learning_rate
+                     , double random_weight_range
+                     )
+    : Predictor_Base(input, learning_rate, random_weight_range, 1)
+    , core( input, number_of_joints, random_weight_range, context)
+    {
+        dbg_msg("Initialize Predictor using homeokinetic controller.");
+    }
+
+    virtual ~Homeokinetic_Core() = default;
+
+    void copy(Predictor_Base const& other) override {
+        Predictor_Base::operator=(other); // copy base members
+        Homeokinetic_Core const& rhs = dynamic_cast<Homeokinetic_Core const&>(other);
+        core = rhs.core;
+        dbg_msg("Copying homeokinetic pred/ctrl weights.");
+    };
+
+    Predictor_Base::vector_t const& get_prediction(void) const override { return core.get_next_state(); }
+
+    double predict(void) override {
+        core.read_next_state(input); //TODO alt: consider to inject, actual motor commands
+        core.predict();
+        core.backup_state();
+        core.control();
+        return calculate_prediction_error();
+    };
+
+    double verify(void) override {
+        core.predict();
+        return calculate_prediction_error();
+    }
+
+    void initialize_randomized(void) override {
+        core.randomize_weights(random_weight_range);
+        prediction_error = predictor_constants::error_min;
+        /*Note: experience buffer not randomized here. not used */
+    };
+
+    void initialize_from_input(void) override { assert(false && "one shot learning not supported."); }
+
+    void draw(void) const { assert(false && "not implemented yet."); }
+
+    void save(std::string /*folder*/) { wrn_msg("FIXME: nothing saved yet."); /*TODO implement */ }
+    void load(std::string /*folder*/) { wrn_msg("FIXME: nothing loaded yet.");/*TODO implement */ }
+
+
+private:
+
+    void learn_from_input_sample(void) override {
+        core.reconstruct();
+        core.adapt_prediction();
+        core.adapt_controller();
+    }
+
+    void learn_from_experience(std::size_t /*skip_idx*/) override {
+        assert(false && "Learning from experience is not implemented yet.");
+    };
+
+
+
+    //friend class Predictor_Graphics;
+};
+
+} /* namespace learning */
+
+#endif /* HOMEOKINETIC_PREDICTOR_H */
+
+
---
+++ src/tests/homeokinetic_core_tests.cpp
@@ -0,0 +1,23 @@
+#include <tests/catch.hpp>
+#include <tests/test_robot.h>
+
+#include <common/log_messages.h>
+#include <control/sensorspace.h>
+#include <learning/predictor.h>
+#include <learning/time_state_space.h>
+#include <learning/homeokinetic_predictor.h>
+#include <controller/pid_control.hpp>
+
+TEST_CASE( "homeokinetic predictor construction", "[homeokinetic_core]" )
+{
+    typedef learning::Homeokinetic_Control::Vector_t ExtInputVector_t;
+    typedef std::vector<supreme::pid_control> PID_joint_control_vector_t;
+
+    Test_Robot robot(5,2);
+    learning::Time_State_Space<8> inputs{robot};
+    PID_joint_control_vector_t    pid;
+    ExtInputVector_t              ext_input;
+
+    //test_space sensors(0.01);
+    learning::Homeokinetic_Core core(inputs, robot.get_joints().size(), /*learning_rate=*/0.01, /*random_weight_range=*/0.1);
+}
